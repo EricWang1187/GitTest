8b5f626a678d94452885b7f58020354963975b0a
Erica <1187559867@qq.com>
2021-05-10 22:05:10 +0800

    first commit

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @Author Erica Wang
+ * @Date 2021/5/10 22:03
+ * @Version 1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
